444a5e8ffb5f8681de9148d7ebc0f08905d12919
sanidhyaanand <sanidhya.anand03@gmail.com>
2022-11-09 20:52:12 +0530

    Repairing SimGNN attention module Refactored and repaired SimGNN attention module to better represent the original formulas from the paper, and switched from singular attention class (modifiable through flags) for every retrieval model to paper-specific module-wise classes to better fit into torch-geometric's structure.

--- sgmatch/modules/attention.py
+++ sgmatch/modules/attention.py
@@ -1,35 +1,39 @@
 import torch
 from torch.functional import Tensor
-from torch.nn.utils.rnn import pad_sequence
-from ..utils.utility import cudavar
 
-class AttentionLayer(torch.nn.Module):
-    def __init__(self, input_dim, type: str = 'simgnn', activation: str = "tanh", a = 0.1):
-        """
-        :param: type: Type of attention mechanism to be used
-        :param: input_dim: Input Dimension of the Node Embeddings
-        :param: activation: The Activation Function to be used for the Attention Layer
-        :param: a: Slope of the -ve part if the activation is Leaky ReLU
-        """
-        super(AttentionLayer, self).__init__()
-        self.type = type
-        self.d = input_dim # Input dimension of the node embeddings
+class GlobalContextAttention(torch.nn.Module):
+    r"""
+    Attention Mechanism layer for the attention operator from the 
+    `"SimGNN: A Neural Network Approach to Fast Graph Similarity Computation"
+    <https://arxiv.org/pdf/1808.05689.pdf>`_ paper
+
+    TODO: Include latex formula for attention computation and aggregation update
+
+    Args:
+        input_dim: 
+        type: Type of attention mechanism to be used
+        input_dim: Input Dimension of the Node Embeddings
+        activation: The Activation Function to be used for the Attention Layer
+        a: Slope of the -ve part if the activation is Leaky ReLU
+    """
+    def __init__(self, input_dim, activation: str = "tanh", a = 0.1):
+        super(GlobalContextAttention, self).__init__()
+        self.input_dim = input_dim
         self.activation = activation 
-        self.a = a # Slope of the negative part in Leaky-ReLU
+        self.a = a 
         
-        self.params()
-        self.initialize()
-        
-    def params(self):
-        if(self.type == 'simgnn'):
-            self.weight_matrix = torch.nn.Parameter(torch.Tensor(self.d, self.d))
+        self.initialize_parameters()
 
-    def initialize(self):
-        """
+    def initialize_parameters(self):
+        r"""
         Weight initialization depends upon the activation function used.
-        If ReLU/ Leaky ReLU : He (Kaiming) Initialization
-        If tanh/ sigmoid : Xavier Initialization
+        If ReLU/Leaky ReLU : He (Kaiming) Initialization
+        If tanh/sigmoid : Xavier Initialization
+
+        TODO: Needs justification/reference
         """
+        self.weight_matrix = torch.nn.Parameter(torch.Tensor(self.input_dim, self.input_dim))
+
         if self.activation == "leaky_relu" or self.activation == "relu":
             torch.nn.init.kaiming_normal_(self.weight_matrix, a = self.a, nonlinearity = self.activation)
         elif self.activation == "tanh" or self.activation == "sigmoid":
@@ -38,18 +42,30 @@ class AttentionLayer(torch.nn.Module):
             raise ValueError("Activation can only take values: 'relu', 'leaky_relu', 'sigmoid', 'tanh';\
                             {} is invalid".format(self.activation))
 
-    def forward(self, node_embeds: Tensor):
-        """ 
-        :param: node_embeds : Node Embedding Tensor of shape N x D
-        :return: global_graph_embedding for each graph in the batch
+    def forward(self, x: Tensor) -> Tensor:
+        r""" 
+        Args:
+            x (torch.Tensor) : Node Embedding Tensor of shape N x D.
+        
+        Returns:
+            representation (torch.Tensor): Global graph representation for input node 
+            representation set.
         """
-        context = torch.mean(torch.matmul(node_embeds, self.weight_matrix), dim = 0)
+        if x.shape[1] != self.input_dim:
+            raise RuntimeError("dim 1 of input tensor does not match dimension of weight matrix")
+        
         activations = {"tanh": torch.nn.functional.tanh, "leaky_relu": torch.nn.functional.leaky_relu,
                         "relu": torch.nn.functional.relu, "sigmoid": torch.nn.functional.sigmoid}
+
+        # Generating the global context
+        global_context = torch.mean(torch.matmul(x, self.weight_matrix), dim = 0)
+
+        # Applying the Non-Linearity over global context vector
         _activation = activations[self.activation]
-        # Applying the Non-Linearity over Weight_matrix*mean(U_i), the default is tanh
-        transformed_context = _activation(context)
-        sigmoid_scores = torch.sigmoid(torch.mm(node_embeds, transformed_context.view(-1, 1)))
-        representation = torch.matmul(torch.t(node_embeds), sigmoid_scores)
+        global_context = _activation(global_context)
+
+        # Computing attention weights and weight-aggregating node embeddings
+        att_weights = torch.sigmoid(torch.matmul(x, global_context.view(-1, 1)))
+        representation = torch.sum(x * att_weights, dim = 0)
         
         return representation
