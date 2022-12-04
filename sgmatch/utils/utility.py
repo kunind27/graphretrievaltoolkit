04d3d4c12761c1e420c113c70b11a46ca35f2fb9
sanidhyaanand <sanidhya.anand03@gmail.com>
2022-12-04 16:49:22 +0530

    Added basic multi layer LRL model creator function

--- sgmatch/utils/utility.py
+++ sgmatch/utils/utility.py
@@ -1,5 +1,19 @@
 import os
+
+import torch
 from torch import cuda
+
+def setup_LRL(input_dim, hidden_sizes):
+    mlp = torch.nn.ModuleList()
+    _in = input_dim
+    for i in range(len(hidden_sizes)):
+        if i != 0:
+            mlp.append(torch.nn.ReLU)
+        _out = hidden_sizes[i]
+        mlp.append(torch.nn.Linear(_in, _out))
+        _in = _out
+    
+    return mlp
 
 def cudavar(x):
     return x.cuda() if cuda.is_available() else x
