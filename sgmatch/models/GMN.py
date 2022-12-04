a8b8e6d6b5fb93714b24e9f146e12103d128dc50
sanidhyaanand <sanidhya.anand03@gmail.com>
2022-12-04 16:53:26 +0530

    Added GMN models

--- sgmatch/models/GMN.py
+++ sgmatch/models/GMN.py
@@ -1,122 +1,148 @@
+from typing import Optional
+
 import torch
-from torch_geometric.nn.conv import GCNConv, SAGEConv
-from torch_geometric.data import Data, Batch
-from torch.nn.utils.rnn import pad_sequence
-from utils.utility import cudavar
-from utils.segment import unsorted_segment_sum
+from torch_geometric.nn.aggr.attention import AttentionalAggregation
 
-# TODO:
+from ..modules.encoder import MLPEncoder
+from ..modules.propagation import GraphProp
+from ..modules.attention import CrossGraphAttention
 
-# 1. Should probably rename this file to GEM to separate it from GMN functionality
-
-class GraphEncoder(torch.nn.Module):
-    """Encoder module that projects node and edge features to some embeddings."""
-    def __init__(self, node_feature_dim: int, edge_feature_dim: int, node_hidden_sizes = None, 
-                 edge_hidden_sizes = None, name='graph-encoder'):
-        """Constructor.
-
-        Args:
-          node_hidden_sizes: if provided should be a list of ints, hidden sizes of
-            node encoder network, the last element is the size of the node outputs.
-            If not provided, node features will pass through as is.
-          edge_hidden_sizes: if provided should be a list of ints, hidden sizes of
-            edge encoder network, the last element is the size of the edge outptus.
-            If not provided, edge features will pass through as is.
-          name: name of this module.
-        """
-        super(GraphEncoder, self).__init__()
-
-        # this also handles the case of an empty list
-        self._node_feature_dim = node_feature_dim
-        self._edge_feature_dim = edge_feature_dim
-        self._node_hidden_sizes = node_hidden_sizes
-        self._edge_hidden_sizes = edge_hidden_sizes
-        self._build_model()
-
-    def _build_model(self):
-        # Build as LRLRL...
-        if self._node_hidden_sizes:
-            layer = []
-            layer.append(torch.nn.Linear(self._node_feature_dim, self._node_hidden_sizes[0]))
-            for i in range(1, len(self._node_hidden_sizes)):
-                layer.append(torch.nn.ReLU())
-                layer.append(torch.nn.Linear(self._node_hidden_sizes[i - 1], self._node_hidden_sizes[i]))
-            self.MLP1 = torch.nn.Sequential(*layer)
-        else:
-            self.MLP1 = None
-
-        if self._edge_hidden_sizes:
-            # unneeded?
-            if not self._edge_feature_dim:
-                raise ValueError("Edge transformation applied but 0 edge dimension given")
-            layer = []
-            layer.append(torch.nn.Linear(self._edge_feature_dim, self._edge_hidden_sizes[0]))
-            for i in range(1, len(self._node_hidden_sizes)):
-                layer.append(torch.nn.ReLU())
-                layer.append(torch.nn.Linear(self._edge_hidden_sizes[i - 1], self._edge_hidden_sizes[i]))
-            self.MLP2 = torch.nn.Sequential(*layer)
-        else:
-            self.MLP2 = None
-
-    def forward(self, node_features, edge_features=None):
-        """Encode node and edge features.
-
-        Args:
-          node_features: [n_nodes, node_feat_dim] float tensor.
-          edge_features: if provided, should be [n_edges, edge_feat_dim] float
-            tensor.
-
-        Returns:
-          node_outputs: [n_nodes, node_embedding_dim] float tensor, node embeddings.
-          edge_outputs: if edge_features is not None and edge_hidden_sizes is not
-            None, this is [n_edges, edge_embedding_dim] float tensor, edge
-            embeddings; otherwise just the input edge_features.
-        """
-        if not self.MLP1:
-            node_outputs = node_features
-        else:
-            node_outputs = self.MLP1(node_features)
-        if edge_features is None or self._edge_hidden_sizes is None:
-            edge_outputs = edge_features
-        else:
-            edge_outputs = self.MLP2(node_features)
-
-        return node_outputs, edge_outputs
-
-def graph_prop_once(node_states, from_idx, to_idx, message_net, 
-                    aggregation_module=None, edge_features=None):
-    """One round of propagation (message passing) in a graph.
-
-    Args:
-      node_states: [n_nodes, node_state_dim] float tensor, node state vectors, one
-        row for each node.
-      from_idx: [n_edges] int tensor, index of the from nodes.
-      to_idx: [n_edges] int tensor, index of the to nodes.
-      message_net: a network that maps concatenated edge inputs to message
-        vectors.
-      aggregation_module: a module that aggregates messages on edges to aggregated
-        messages for each node.  Should be a callable and can be called like the
-        following,
-        `aggregated_messages = aggregation_module(messages, to_idx, n_nodes)`,
-        where messages is [n_edges, edge_message_dim] tensor, to_idx is the index
-        of the to nodes, i.e. where each message should go to, and n_nodes is an
-        int which is the number of nodes to aggregate into.
-      edge_features: if provided, should be a [n_edges, edge_feature_dim] float
-        tensor, extra features for each edge.
-
-    Returns:
-      aggregated_messages: an [n_nodes, edge_message_dim] float tensor, the
-        aggregated messages, one row for each node.
+class GMNEmbed(torch.nn.Module):
+    r"""
     """
-    from_states = node_states[from_idx]
-    to_states = node_states[to_idx]
-    edge_inputs = [from_states, to_states]
+    # TODO: Provide default arguments for MLP layer sizes
+    def __init__(self, node_feature_dim: int, enc_node_hidden_sizes: List[int], 
+                prop_node_hidden_sizes: List[int], prop_message_hidden_sizes: List[int],
+                aggr_gate_hidden_sizes: List[int], aggr_mlp_hidden_sizes: List[int], 
+                edge_feature_dim: Optional[int] = None, enc_edge_hidden_sizes: Optional[List[int]] = None,
+                message_net_init_scale: float = 0.1, node_update_type: str = 'residual', 
+                use_reverse_direction: bool = True, reverse_dir_param_different: bool = True, 
+                layer_norm: bool = False):
+        super(GMNEmbed, self).__init__()
+        self.node_feature_dim = node_feature_dim
+        self.edge_feature_dim = edge_feature_dim
 
-    if edge_features is not None:
-        edge_inputs.append(edge_features)
+        # Encoder Module        
+        self.enc_node_layers = enc_node_hidden_sizes
+        self.enc_edge_layers = enc_edge_hidden_sizes
+        
+        # Propagation Module
+        self.prop_node_layers = prop_node_hidden_sizes
+        self.prop_message_layers = prop_message_hidden_sizes
+        
+        # Aggregation Module
+        self.aggr_gate_layers = aggr_gate_hidden_sizes + [node_feature_dim]
+        self.aggr_mlp_layers = aggr_mlp_hidden_sizes + [node_feature_dim]
 
-    edge_inputs = torch.cat(edge_inputs, dim=-1)
-    messages = message_net(edge_inputs)
+        self.message_net_init_scale = message_net_init_scale # Unused
+        self.node_update_type = node_update_type
+        self.use_reverse_direction = use_reverse_direction
+        self.reverse_dir_param_different = reverse_dir_param_different
 
-    tensor = unsorted_segment_sum(messages, to_idx, node_states.shape[0])
-    return tensor
+        self.layer_norm = layer_norm
+        self.prop_type = "embedding"
+        
+        self.setup_layers()
+
+    def setup_layers(self):
+        self._encoder = MLPEncoder(node_feature_dim, node_hidden_sizes, edge_feature_dim=edge_feature_dim, 
+                            edge_hidden_sizes=edge_hidden_sizes)
+        self._propagator = GraphProp(self.node_feature_dim, self.prop_node_layers, self.prop_message_layers, 
+                               edge_feature_dim=self.edge_feature_dim, message_net_init_scale=self.message_net_init_scale,
+                               node_update_type=self.node_update_type, use_reverse_direction=self.use_reverse_direction,
+                               reverse_dir_param_different=self.reverse_dir_param_different, layer_norm=self.layer_norm,
+                               prop_type=self.prop_type)        
+        
+        # Setup aggregator MLPs
+        self.aggr_gate = setup_LRL(self.node_feature_dim, self.aggr_gate_layers)
+        self.aggr_mlp = setup_LRL(self.node_feature_dim, self.aggr_mlp_layers)
+
+        self._aggregator = AttentionalAggregation(self.aggr_gate, self.aggr_mlp)
+
+    def forward(self, node_features: Tensor, edge_index: Tensor, edge_features: Optional[Tensor] = None,
+                num_prop: int = 10):
+        from_idx = edge_index[:,0] if len(edge_index.shape) == 3 else edge_index[0]
+        to_idx = edge_index[:,1] if len(edge_index.shape) == 3 else edge_index[1]
+
+        if edge_features is not None:
+            node_features, edge_features = self._encoder(node_features, edge_features)
+        else:
+            node_features = self._encoder(node_features)
+        
+        for _ in range(num_prop):
+            # TODO: Can include a list keeping track of propagation layer outputs
+            node_features = self._propagator(node_features, from_idx, to_idx, edge_features)
+
+        return self._aggregator(node_features)
+
+class GMNMatch(torch.nn.Module):
+    r"""
+    """
+    # TODO: Provide default arguments for MLP layer sizes
+    def __init__(self, node_feature_dim: int, enc_node_hidden_sizes: List[int], 
+                prop_node_hidden_sizes: List[int], prop_message_hidden_sizes: List[int],
+                aggr_gate_hidden_sizes: List[int], aggr_mlp_hidden_sizes: List[int], 
+                edge_feature_dim: Optional[int] = None, enc_edge_hidden_sizes: Optional[List[int]] = None,
+                message_net_init_scale: float = 0.1, node_update_type: str = 'residual', 
+                use_reverse_direction: bool = True, reverse_dir_param_different: bool = True, 
+                attention_sim_metric: str = "euclidean", layer_norm: bool = False):
+        super(GMNEmbed, self).__init__()
+        self.node_feature_dim = node_feature_dim
+        self.edge_feature_dim = edge_feature_dim
+
+        # Encoder Module        
+        self.enc_node_layers = enc_node_hidden_sizes
+        self.enc_edge_layers = enc_edge_hidden_sizes
+        
+        # Propagation Module
+        self.prop_node_layers = prop_node_hidden_sizes
+        self.prop_message_layers = prop_message_hidden_sizes
+        
+        # Aggregation Module
+        self.aggr_gate_layers = aggr_gate_hidden_sizes + [node_feature_dim]
+        self.aggr_mlp_layers = aggr_mlp_hidden_sizes + [node_feature_dim]
+
+        self.message_net_init_scale = message_net_init_scale # Unused
+        self.node_update_type = node_update_type
+        self.use_reverse_direction = use_reverse_direction
+        self.reverse_dir_param_different = reverse_dir_param_different
+
+        self.attention_sim_metric = attention_sim_metric
+        self.layer_norm = layer_norm
+        self.prop_type = "matching"
+        
+        self.setup_layers()
+
+    def setup_layers(self):
+        self._encoder = MLPEncoder(node_feature_dim, node_hidden_sizes, edge_feature_dim=edge_feature_dim, 
+                            edge_hidden_sizes=edge_hidden_sizes)
+        self._attention = CrossGraphAttention(similarity_metric=self.attention_sim_metric)
+        self._propagator = GraphProp(self.node_feature_dim, self.prop_node_layers, self.prop_message_layers, 
+                               edge_feature_dim=self.edge_feature_dim, message_net_init_scale=self.message_net_init_scale,
+                               node_update_type=self.node_update_type, use_reverse_direction=self.use_reverse_direction,
+                               reverse_dir_param_different=self.reverse_dir_param_different, layer_norm=self.layer_norm,
+                               prop_type=self.prop_type)        
+
+        # Setup aggregator MLPs
+        self.aggr_gate = setup_LRL(self.node_feature_dim, self.aggr_gate_layers)
+        self.aggr_mlp = setup_LRL(self.node_feature_dim, self.aggr_mlp_layers)
+
+        self._aggregator = AttentionalAggregation(self.aggr_gate, self.aggr_mlp)
+
+    def forward(self, node_features_i: Tensor, node_features_j: Tensor, edge_index_i: Tensor, edge_index_j: Tensor, 
+                edge_features_i: Optional[Tensor] = None, num_prop: int = 10):
+        r"""
+        """
+        from_idx = edge_index[:,0] if len(edge_index.shape) == 3 else edge_index[0]
+        to_idx = edge_index[:,1] if len(edge_index.shape) == 3 else edge_index[1]
+
+        if edge_features is not None:
+            node_features, edge_features = self._encoder(node_features, edge_features)
+        else:
+            node_features = self._encoder(node_features)
+        
+        for _ in range(num_prop):
+            # TODO: Can include a list keeping track of propagation layer outputs
+            node_features = self._propagator(node_features, from_idx, to_idx, node_features_j, edge_features)
+
+        return self._aggregator(node_features)
