eade5313e03d56fd349a6b83bbba347f9bc9a962
sanidhyaanand <sanidhya.anand03@gmail.com>
2022-12-10 02:21:56 +0530

    added conv_setup utility function

--- sgmatch/utils/utility.py
+++ sgmatch/utils/utility.py
@@ -1,7 +1,9 @@
 from typing import List
 
 import torch
-from torch import cuda
+
+from constants import CONVS, ACTIVATIONS
+# from torch import cuda
 
 def setup_linear_nn(input_dim: int, hidden_sizes: List[int]):
     mlp = torch.nn.ModuleList()
@@ -14,9 +16,11 @@ def setup_linear_nn(input_dim: int, hidden_sizes: List[int]):
     return mlp
 
 def setup_LRL_nn(input_dim: int, hidden_sizes: List[int], 
-                 activation: torch.nn.Module = torch.nn.ReLU):
+                 activation: str = "relu"):
+    # XXX: Better to leave this up to MLP class?
     mlp = []
     _in = input_dim
+    activation = ACTIVATIONS[activation]
     for i in range(len(hidden_sizes) - 1):
         _out = hidden_sizes[i]
         mlp.append(torch.nn.Linear(_in, _out))
@@ -26,6 +30,18 @@ def setup_LRL_nn(input_dim: int, hidden_sizes: List[int],
     mlp = torch.nn.Sequential(*mlp)
     
     return mlp
+
+def setup_conv_layers(input_dim, conv_type, filters):
+    convs = torch.nn.ModuleList()
+    _conv = CONVS[conv_type]
+    num_layers = len(filters)
+    _in = input_dim
+    for i in range(num_layers):
+        _out = filters[i]
+        convs.append(_conv(in_channels=_in, out_channels=_out))
+        _in = _out
+
+    return convs
 
 # def cudavar(x):
 #     return x.cuda() if cuda.is_available() else x
